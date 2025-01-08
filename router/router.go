690eaf8d0a7ec67a8f4a507e72137bd1ac045bff
Marcus Costa <marcuskta@icloud.com>
2025-01-08 11:31:39 -0300

    feat: add routes project

--- router/router.go
+++ router/router.go
@@ -4,15 +4,11 @@ import "github.com/gin-gonic/gin"
 
 func Initializa() {
 
-	// inicializa a router utilizando as configurações default do gin
+	// initialize router
 	router := gin.Default()
 
-	// define uma rota
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// initialize routes
+	initializeRoutes(router)
 
 	// linsten and serve on 0.0.0.0:8080
 	router.Run(":8080")
