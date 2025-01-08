c1407b91216fd0a75c177dc548bc0b141b47ce49
Marcus Costa <marcuskta@icloud.com>
2025-01-08 15:49:51 -0300

    feat: add createOpening handler project

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/costamarcus/go-portunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
