0dcf672cb30497f70356f8b8b262676c9ba21b61
Marcus Costa <marcuskta@icloud.com>
2025-01-08 12:41:16 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
-import "github.com/costamarcus/go-portunities/router"
+import (
+	"github.com/costamarcus/go-portunities/config"
+	"github.com/costamarcus/go-portunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	
+
+	logger = config.GetLogger("main")
+
+	// initialize config
+	err := config.Init()
+	if err != nil {
+		logger.ErrorF("config initilization error: %v", err)
+		return
+	}
+
 	// initialize router
 	router.Initializa()
 }
