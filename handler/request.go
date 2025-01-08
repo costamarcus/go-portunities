455965836dc8f960fe10ab1772384a13b8a414fa
Marcus Costa <marcuskta@icloud.com>
2025-01-08 16:59:03 -0300

    feat: add update opening handler logic

--- handler/request.go
+++ handler/request.go
@@ -46,3 +46,23 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 	return nil
 }
+
+// updateOpening
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validade() error {
+	// if any field is provided, validation is truthy
+	if r.Role == "" || r.Company == "" || r.Location == "" || r.Link == "" || r.Remote == nil || r.Salary > 0 {
+		return nil
+	}
+
+	// if none of the fields were provided, return false
+	return fmt.Errorf("at least one valid field must be provided")
+}
